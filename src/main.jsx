714f93c15d0c46ef1e7c6d21c07b58047189d0eb
DirtyAndShaggy <bornonlyforgaming@gmail.com>
2026-02-09 00:20:10 +0700

    trying to deploy in vercel take 4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { SettingsProvider } from "@/Context/SettingsContext";
+import { SettingsProvider } from "@/context/SettingsContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
